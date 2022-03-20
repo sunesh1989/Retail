45349d6a9aec5007858fa10408a93fd05b235384
sunesh1989 <suneshkumarp@gmail.com>
2022-03-21 00:36:05 +0530

    Added Test project

---
+++ RetailManagementAPI.UnitTest/ProductControllerTest.cs
@@ -0,0 +1,48 @@
+using EasyNetQ;
+using Microsoft.Extensions.Configuration;
+using RetailManagement.Shared.Product;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace RetailManagementAPI.UnitTest
+{
+    public class ProductControllerTest
+    {
+        private const string RabbitMqConnection = "host=localhost";
+        
+        [Fact]
+        public void GetAllProduct()
+        {
+            var messageBus = RabbitHutch.CreateBus(RabbitMqConnection);
+            var reponse = messageBus.Rpc.Request<ProductRequest, ProductResponse>(new ProductRequest
+            {
+                Records = 5
+            });
+            Assert.True(reponse.Products.Count > 0);
+        }
+
+        [Fact]
+        public void GetSpecificProduct()
+        {
+            var messageBus = RabbitHutch.CreateBus(RabbitMqConnection);
+            var products = messageBus.Rpc.Request<ProductRequest, ProductResponse>(new ProductRequest
+            {
+                Records = 5
+            });
+            if (products.Products.Count > 0)
+            {
+                var firstProductId = products.Products.First().Id;
+                var reponse = messageBus.Rpc.Request<ProductItemRequest, ProductResponse>(new ProductItemRequest
+                {
+                    Parameter = firstProductId
+                });
+                Assert.True(reponse.Product != null && reponse.Product.Id == firstProductId);
+            }
+            else
+            {
+                Assert.False(true);
+            }
+        }
+    }
+}
